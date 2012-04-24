17cbe2c5057e0bb1231a70461e8b0a08e0b8baef
Steven Davidovitz <sdavidovitz@zendesk.com>
2012-04-24 12:42:54 -0700

    add example.rb

---
+++ example.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env bundle exec ruby
+require 'zendesk'
+
+client = Zendesk.configure do |config|
+  config.username = "agent@zendesk.com"
+  config.password = 123456
+  config.url = "http://dev.localhost:3000/api/v2/"
+  config.log = true
+  config.retry = true
+end
+
+users = client.users
+puts users.inspect
+users.fetch
+puts users.first.inspect
