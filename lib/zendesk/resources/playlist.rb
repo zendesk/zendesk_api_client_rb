90ebda063867450410ce0445e0b8b5f4def5cbf0
Steven Davidovitz <sdavidovitz@zendesk.com>
2012-04-25 15:14:00 -0700

    add playlist error handling, playlist tests

--- lib/zendesk/resources/playlist.rb
+++ lib/zendesk/resources/playlist.rb
@@ -1,22 +1,29 @@
 module Zendesk
   class Playlist
-    attr_reader :ticket, :id
+    attr_reader :ticket
+    attr_accessor :id
+
     def initialize(client, id)
       @client, @id = client, id
       @ticket = nil
 
-      response = @client.connection.get("views/#{id}/play.json")
-      @destroyed = response.status != 302
+      @initialized = false
+      @destroyed = false
+
+      init_playlist
     end
 
     def each
-      while !@destroyed
-        yield self.next
+      init_playlist unless initialized?
+
+      while initialized? && !destroyed? && (n = self.next)
+        yield n
       end
     end
 
     def next
-      return false if @destroyed
+      init_playlist unless initialized?
+      return false if !initialized? || destroyed? 
 
       response = @client.connection.get("play/next.json")
 
@@ -24,14 +31,34 @@ module Zendesk
         @ticket = Ticket.new(@client, response.body["ticket"])
         @ticket
       else
-        # Depends, but definitely if 204
-        @destroyed = response.status == 204 
+        @destroyed = response.status == 204
+        nil
       end
+    rescue Faraday::Error::ClientError
+      nil
     end
 
     def destroy
       response = @client.connection.delete("play.json")
       @destroyed = response.status == 204 
+    rescue Faraday::Error::ClientError
+      false
+    end
+
+    def destroyed?
+      @destroyed
+    end
+
+    def initialized?
+      @initialized
+    end
+
+    private
+
+    def init_playlist
+      response = @client.connection.get("views/#{id}/play.json")
+      @initialized = response.status == 302
+    rescue Faraday::Error::ClientError
     end
   end
 end
