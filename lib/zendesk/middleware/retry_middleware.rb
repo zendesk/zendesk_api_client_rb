b0f50f45c3d36b6ea45fe06c9831ec0f5cf1b57e
Steven Davidovitz <sdavidovitz@zendesk.com>
2012-04-25 14:45:58 -0700

    fix retry spec. add 503 handling

--- lib/zendesk/middleware/retry_middleware.rb
+++ lib/zendesk/middleware/retry_middleware.rb
@@ -1,12 +1,12 @@
 module Zendesk
   module Request
-    # Faraday middleware to handle HTTP Status 429 (rate limiting).
+    # Faraday middleware to handle HTTP Status 429 (rate limiting) / 503 (maintenance)
     class RetryMiddleware < Faraday::Middleware
       def call(env)
         response = @app.call(env)
 
-        if response.env[:status] == 429
-          seconds_left = response.env[:response_headers][:retry_after].to_i
+        if [429, 503].include?(response.env[:status])
+          seconds_left = (response.env[:response_headers][:retry_after] || 10).to_i
           end_time = Time.now + seconds_left + 1
           print "You have been rate limited. Retrying in #{seconds_left} seconds..."
           
--- spec/retry_middleware_spec.rb
+++ spec/retry_middleware_spec.rb
@@ -3,15 +3,29 @@ require 'spec_helper'
 describe Zendesk::Request::RetryMiddleware, :vcr_off do
   let(:client) { valid_client }
 
-  before(:each) do
-    stub_request(:get, %r{blergh}).to_return(:status => 429,
-                                                     :headers => { :retry_after => 1 }).
-                                                     to_return(:status => 200)
+
+  context "on 429 [rate-limit]" do
+    before(:each) do
+      stub_request(:get, %r{blergh}).to_return(:status => 429,
+                                               :headers => { :retry_after => 1 }).
+                                               to_return(:status => 200)
+    end
+
+    it "should wait requisite seconds and then retry request" do
+      client.connection.get("blergh").status.should == 200
+    end
   end
 
-  it "should wait requisite seconds and then retry request" do
-    Zendesk::Request::RetryMiddleware.any_instance.should_receive(:sleep).at_least(:once)
-    client.connection.get("blergh").status.should == 200
+  context "on 503" do
+    before(:each) do
+      stub_request(:get, %r{blergh}).to_return(:status => 503).
+        to_return(:status => 200)
+    end
+
+    it "should wait 10 seconds and then retry request" do
+      client.connection.get("blergh").status.should == 200
+    end
   end
+
 
 end
