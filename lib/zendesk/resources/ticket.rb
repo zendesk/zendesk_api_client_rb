79e6f54358c8b9dd4d70efc049b32cd140d4c72e
Steven Davidovitz <sdavidovitz@zendesk.com>
2012-05-07 16:26:26 -0700

    add new resources

--- lib/zendesk/resources/ticket.rb
+++ lib/zendesk/resources/ticket.rb
@@ -2,6 +2,7 @@ module Zendesk
   class TicketField < Resource; end
   class Audit < DataResource; end
   class TicketComment < Data; end
+  class TicketMetric < ReadResource; end
 
   class Ticket < Resource
     class Audit < DataResource; end
@@ -11,12 +12,18 @@ module Zendesk
     has :recipient, :class => :user
     has_many :collaborators, :class => :user
     has_many :audits
+    has_many :metrics, :class => :ticket_metric
     has :group
     has :forum_topic, :class => :topic
     has :organization
 
     has_many :uploads, :class => :attachment, :save => true
     has :comment, :class => :ticket_comment, :save => true
+  end
+
+  class SuspendedTicket < ReadResource
+    extend Destroy
+    put :recover
   end
 
   class View < DataResource
