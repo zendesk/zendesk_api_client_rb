7ff37ae637cca385e0e3651313a055cd080d6740
grosser <grosser.michael@gmail.com>
2012-05-29 11:57:20 -0700

    refactor retry middleware so we can have faster tests

--- lib/zendesk/middleware/retry_middleware.rb
+++ lib/zendesk/middleware/retry_middleware.rb
@@ -2,17 +2,18 @@ module Zendesk
   module Request
     # Faraday middleware to handle HTTP Status 429 (rate limiting) / 503 (maintenance)
     class RetryMiddleware < Faraday::Middleware
+      DEFAULT_TIMEOUT = 10
+
       def call(env)
         response = @app.call(env)
 
         if [429, 503].include?(response.env[:status])
-          seconds_left = (response.env[:response_headers][:retry_after] || 10).to_i
-          end_time = Time.now + seconds_left + 1
+          seconds_left = (response.env[:response_headers][:retry_after] || DEFAULT_TIMEOUT).to_i
           print "You have been rate limited. Retrying in #{seconds_left} seconds..."
-          
-          until end_time <= Time.now
-            sleep(1)
-            time_left = (end_time - Time.now).to_i
+
+          seconds_left.times do |i|
+            sleep 1
+            time_left = seconds_left - i
             print "#{time_left}..." if time_left > 0 && time_left % 5 == 0
           end
 
--- spec/middleware/retry_middleware_spec.rb
+++ spec/middleware/retry_middleware_spec.rb
@@ -1,26 +1,33 @@
 require 'spec_helper'
 
 describe Zendesk::Request::RetryMiddleware, :vcr_off do
-  context "on 429 [rate-limit]" do
-    before(:each) do
-      stub_request(:get, %r{blergh}).to_return(:status => 429,
-                                               :headers => { :retry_after => 1 }).
-                                               to_return(:status => 200)
-    end
+  def runtime
+    start = Time.now.to_f
+    yield
+    Time.now.to_f - start
+  end
 
-    it "should wait requisite seconds and then retry request" do
-      client.connection.get("blergh").status.should == 200
+  [429, 503].each do |error_code|
+    it "should wait requisite seconds and then retry request on #{error_code}" do
+      stub_request(:get, %r{blergh}).
+        to_return(:status => 429, :headers => { :retry_after => 1 }).
+        to_return(:status => 200)
+
+      runtime do
+        client.connection.get("blergh").status.should == 200
+      end.should be_within(0.2).of(1)
     end
   end
 
-  context "on 503" do
-    before(:each) do
-      stub_request(:get, %r{blergh}).to_return(:status => 503).
-        to_return(:status => 200)
-    end
+  it "should wait default timeout seconds and then retry request on error" do
+    stub_request(:get, %r{blergh}).
+      to_return(:status => 503).
+      to_return(:status => 200)
 
-    it "should wait 10 seconds and then retry request" do
+    Zendesk::Request::RetryMiddleware.any_instance.should_receive(:sleep).exactly(10).times.with(1)
+
+    runtime do
       client.connection.get("blergh").status.should == 200
-    end
+    end.should <= 0.5
   end
 end
