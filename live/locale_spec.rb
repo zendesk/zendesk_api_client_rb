5f809f9788641efb50d4dbf0e4254a90c6599b4b
grosser <grosser.michael@gmail.com>
2012-05-29 17:05:54 -0700

    fix locales spec

--- live/locale_spec.rb
+++ live/locale_spec.rb
@@ -4,7 +4,7 @@ describe Zendesk::Locale do
   use_vcr_cassette
 
   specify "client#current_locale" do
-    client.locale.should be_instance_of(described_class)
+    client.current_locale.should be_instance_of(described_class)
   end
 
   it_should_be_readable :locales
