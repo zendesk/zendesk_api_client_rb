3b9d06296b1d108acfed5b6077c9d12f979e0e5c
grosser <grosser.michael@gmail.com>
2012-06-05 13:12:22 -0700

    fix group membership test

--- live/group_membership_spec.rb
+++ live/group_membership_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe Zendesk::GroupMembership, :delete_after do
+  before :all do
+    VCR.use_cassette("delete_existing_group_memberships_create") do
+      agent.group_memberships.each(&:destroy)
+    end
+  end
+
   def valid_attributes
     { :group_id => group.id, :user_id => agent.id }
   end
@@ -8,5 +14,5 @@ describe Zendesk::GroupMembership, :delete_after do
   it_should_be_creatable
   it_should_be_deletable
   it_should_be_readable :group_memberships
-  it_should_be_readable agent, :group_memberships
+  it_should_be_readable agent, :group_memberships, :create => true
 end
