e5706548d33004753164f1851bd1c83150c401d8
grosser <grosser.michael@gmail.com>
2012-06-19 12:04:06 -0700

    indent

--- lib/zendesk_api/lru_cache.rb
+++ lib/zendesk_api/lru_cache.rb
@@ -1,39 +1,38 @@
 module ZendeskAPI
-end
+  # http://codesnippets.joyent.com/posts/show/12329
+  class ZendeskAPI::LRUCache
+    attr_accessor :size
 
-# http://codesnippets.joyent.com/posts/show/12329
-class ZendeskAPI::LRUCache
-  attr_accessor :size
-
-  def initialize(size = 10)
-    @size = size
-    @store = {}
-    @lru = []
-  end
-
-  def write(key, value)
-    @store[key] = value
-    set_lru(key)
-    @store.delete(@lru.pop) if @lru.size > @size
-    value
-  end
-
-  def read(key)
-    set_lru(key)
-    @store[key]
-  end
-
-  def fetch(key)
-    if @store.has_key? key
-      read key
-    else
-      write key, yield
+    def initialize(size = 10)
+      @size = size
+      @store = {}
+      @lru = []
     end
-  end
 
-  private
+    def write(key, value)
+      @store[key] = value
+      set_lru(key)
+      @store.delete(@lru.pop) if @lru.size > @size
+      value
+    end
 
-  def set_lru(key)
-    @lru.unshift(@lru.delete(key) || key)
+    def read(key)
+      set_lru(key)
+      @store[key]
+    end
+
+    def fetch(key)
+      if @store.has_key? key
+        read key
+      else
+        write key, yield
+      end
+    end
+
+    private
+
+    def set_lru(key)
+      @lru.unshift(@lru.delete(key) || key)
+    end
   end
 end
