354c96910dd1b0532286c457469e5d319d18ddff
grosser <grosser.michael@gmail.com>
2012-07-11 09:20:45 -0700

    find by email;

--- live/user_spec.rb
+++ live/user_spec.rb
@@ -10,4 +10,10 @@ describe ZendeskAPI::User, :delete_after do
   it_should_be_deletable :find => [:active?, false]
   it_should_be_readable :users
   it_should_be_readable organization, :users
+
+  it "should be able to find by email" do
+    VCR.use_cassette("user_find_by_email") do
+      client.users.search(:query => current_user.email).to_a.should == [current_user]
+    end
+  end
 end
