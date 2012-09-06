8cd6d5e4fb1ca0ee56e670e430ebcde02346a34b
Steven Davidovitz <sdavidovitz@zendesk.com>
2012-09-06 14:13:47 -0700

    fix group spec for user#groups

--- live/group_spec.rb
+++ live/group_spec.rb
@@ -10,5 +10,22 @@ describe ZendeskAPI::Group do
   it_should_be_deletable :find => [:deleted?, true]
   it_should_be_readable :groups
   it_should_be_readable :groups, :assignable
-  it_should_be_readable agent, :groups
+
+  context "with a membership" do
+    before(:each) do
+      VCR.use_cassette("read_ZendeskAPI::User_groups_create") do
+        @object = described_class.create!(client, valid_attributes.merge(default_options))
+        @membership = agent.group_memberships.create(:group_id => @object.id, :user_id => agent.id)
+      end
+    end
+
+    after(:each) do
+      VCR.use_cassette("read_ZendeskAPI::User_groups_delete") do
+        @object.destroy
+        @membership.destroy
+      end
+    end
+
+    it_should_be_readable agent, :groups
+  end
 end
