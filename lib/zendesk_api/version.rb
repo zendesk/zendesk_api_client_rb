f5d8c66f2f50144bf7549a614ed5f5a1737f019d
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-05-29 12:42:31 +0200

    fix Etag caching middleware for non-ruby caches

--- lib/zendesk_api/version.rb
+++ lib/zendesk_api/version.rb
@@ -1,3 +1,3 @@
 module ZendeskAPI
-  VERSION = "0.3.6"
+  VERSION = "0.3.7"
 end
--- spec/core/middleware/request/etag_cache_spec.rb
+++ spec/core/middleware/request/etag_cache_spec.rb
@@ -9,9 +9,12 @@ describe ZendeskAPI::Middleware::Request::EtagCache do
     response.status.should == 200
     response.body.should == {"x"=>1}
 
+    headers = response.headers
+
     stub_request(:get, %r{blergh}).to_return(:status => 304, :headers => {"Etag" => "x"})
     response = client.connection.get("blergh")
     response.status.should == 304
     response.body.should == {"x"=>1}
+    response.headers.should == headers
   end
 end
