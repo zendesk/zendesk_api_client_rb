dbcce9ebe1e6717e6e633a73a360a01b5cc43ad6
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-06-10 13:07:58 -0700

    fix ticket activity live spec

--- spec/live/activity_spec.rb
+++ spec/live/activity_spec.rb
@@ -1,5 +1,12 @@
 require 'core/spec_helper'
 
 describe ZendeskAPI::Activity do
+  before do
+    VCR.use_cassette("ticket_activity") do
+      ticket.comment = { :value => "test", :author_id => agent.id }
+      ticket.save!
+    end
+  end
+
   it_should_be_readable :activities
 end
