e608dbc1f28ab6b4934645b28fb93c77a2c22d41
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-06-18 15:37:36 -0700

    clear_changes after setting side-loads after find

--- lib/zendesk_api/actions.rb
+++ lib/zendesk_api/actions.rb
@@ -96,6 +96,7 @@ module ZendeskAPI
 
       new(client, response.body[singular_resource_name]).tap do |resource|
         resource.set_includes(resource, includes, response.body)
+        resource.attributes.clear_changes
       end
     end
 
