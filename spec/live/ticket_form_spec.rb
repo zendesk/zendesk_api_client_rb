5c5feb311c6b0fa130d5c46d4b116cd899dc4a9c
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-09-13 12:26:34 -0700

    fix ticket form live spec

--- spec/live/ticket_form_spec.rb
+++ spec/live/ticket_form_spec.rb
@@ -2,12 +2,12 @@ require 'core/spec_helper'
 
 describe ZendeskAPI::TicketForm, :delete_after do
   def valid_attributes
-    { :name => "Ticket Form-o", :position => 9999 }
+    { :name => "Ticket Form-o", :position => 9999, :active => false }
   end
 
   it_should_be_creatable
   it_should_be_updatable :name
-  it_should_be_deletable
+  it_should_be_deletable :find => false # Deleted ticket forms are still returned from the show action
   it_should_be_readable :ticket_forms
 
   # TODO: clone
