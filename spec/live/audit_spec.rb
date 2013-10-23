5b689f7b103ef0d94aa226ec89e2177decfb2cfb
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-10-22 23:01:20 -0700

    add live AuditEvent test

--- spec/live/audit_spec.rb
+++ spec/live/audit_spec.rb
@@ -2,4 +2,14 @@ require 'core/spec_helper'
 
 describe ZendeskAPI::Ticket::Audit do
   it_should_be_readable ticket, :audits
+
+  describe ZendeskAPI::Ticket::Audit::Event, :vcr do
+    it "should side-load events" do
+      audit = ticket.audits(include: :users).first
+      event = audit.events.first
+
+      event.should be_instance_of(ZendeskAPI::Ticket::Audit::Event)
+      event.author.should be_instance_of(ZendeskAPI::User)
+    end
+  end
 end
