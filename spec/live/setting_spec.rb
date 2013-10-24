e9d0e4aca028ce9e075de1774e5b1b6a382c7ebd
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-10-24 11:29:28 -0700

    add a live test

--- spec/live/setting_spec.rb
+++ spec/live/setting_spec.rb
@@ -2,4 +2,24 @@ require 'core/spec_helper'
 
 describe ZendeskAPI::Setting do
   it_should_be_readable :settings, :path => 'account/settings'
+
+  under (user = ZendeskAPI::User.new(client, :id => 'me')) do
+    it_should_be_readable user, :settings
+
+    describe 'updating', :vcr do
+      it 'should be updatable' do
+        settings = user.settings
+        lotus = settings.detect {|set| set.on == "lotus"}
+
+        original_setting = lotus.keyboard_shortcuts_enabled
+        lotus.keyboard_shortcuts_enabled = !original_setting
+
+        settings.save!
+        settings.fetch!(true)
+
+        lotus = settings.detect {|set| set.on == "lotus"}
+        lotus.keyboard_shortcuts_enabled.should == !original_setting
+      end
+    end
+  end
 end
