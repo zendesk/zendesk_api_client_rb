839a9f1f3cc7d5193d736c5e56f87ec6f3c71a2a
Po Chen <pchen@zendesk.com>
2014-02-12 16:15:15 +1100

    Update live spec to delete app

--- spec/live/app_spec.rb
+++ spec/live/app_spec.rb
@@ -12,20 +12,7 @@ describe ZendeskAPI::App do
 
     VCR.use_cassette("app_create") { app.save! }
 
-    body = {}
-
-    VCR.use_cassette("app_create_job_status") do
-      until %w{failed completed}.include?(body["status"])
-        response = client.connection.get(app.response.headers["Location"])
-        body = response.body
-
-        sleep(3)
-      end
-    end
-
-    if body["status"] == "failed"
-      fail "Could not create app: #{body.inspect}"
-    end
+    check_job app
 
     app.id = body["app_id"]
     app.author_name = "Mr. Sprinkles"
@@ -40,7 +27,28 @@ describe ZendeskAPI::App do
 
   it "should be able to handle the simplest creation api call" do
     VCR.use_cassette("app_simple_create") do
-      expect { ZendeskAPI::App.create!(client, { :name => "Testing App Creation", :upload => "spec/fixtures/sample_app.zip" }) }.to_not raise_error
+      app = ZendeskAPI::App.create!(client, { :name => "Testing App Creation", :upload => "spec/fixtures/sample_app.zip" })
+
+      check_job app
+
+      VCR.use_cassette("app_destroy") { app.destroy! }
     end
+  end
+end
+
+def check_job(app)
+  body = {}
+
+  VCR.use_cassette("app_create_job_status") do
+    until %w{failed completed}.include?(body["status"])
+      response = client.connection.get(app.response.headers["Location"])
+      body = response.body
+
+      sleep(3)
+    end
+  end
+
+  if body["status"] == "failed"
+    fail "Could not create app: #{body.inspect}"
   end
 end
