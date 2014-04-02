bffcdb1190da31581cc386911204b1a7ddd4876b
Jason Dugdale <jason.dugdale@gmail.com>
2014-04-02 14:44:39 +0100

    Basic spec for TopicComment import

--- spec/live/topic_comment_spec.rb
+++ spec/live/topic_comment_spec.rb
@@ -11,6 +11,22 @@ describe ZendeskAPI::Topic::TopicComment do
     it_should_be_deletable
     it_should_be_readable topic, :comments
   end
+
+  describe ".import" do
+    it "can import" do
+      VCR.use_cassette("topic_comment_import_can_import") do
+        old = Time.now - 4*365*24*60*60
+        comment = ZendeskAPI::Topic::TopicComment.import(client, valid_attributes.merge(:created_at => old, :topic_id => topic.id))
+        ZendeskAPI::Topic::TopicComment.find(client, comment).created_at.year.should == old.year
+      end
+    end
+
+    it "returns nothing if import fails" do
+      VCR.use_cassette("topic_comment_import_cannot_import") do
+        silence_logger { ZendeskAPI::Topic::TopicComment.import(client, {}).should == nil }
+      end
+    end
+  end
 end
 
 describe ZendeskAPI::User::TopicComment do
