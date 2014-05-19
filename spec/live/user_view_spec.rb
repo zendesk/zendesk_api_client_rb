f9226fef7afe3295d760580cc35e7c69f570aca2
PC Dhulipalla <pchaitanya@zendesk.com>
2014-05-19 11:31:53 -0700

    Fixing spec

--- spec/live/user_view_spec.rb
+++ spec/live/user_view_spec.rb
@@ -3,19 +3,14 @@ require 'core/spec_helper'
 describe ZendeskAPI::UserView, :delete_after do
   def valid_attributes
     {
-      :title => "my test view",
-      :conditions => {
-        :all => [{ :field => "status", :operator => "is", :value => "open" }]
-      }
+      :title => "Overseas gold member",
+      :all   => [
+        { field: "name", operator: "is", value: "abcd" },
+      ]
     }
   end
 
-  it_should_be_readable :user_views
-
   it_should_be_creatable
-  it_should_be_updatable :conditions, {
-    "any" => [],
-    "all" => [{ "field" => "status", "operator" => "is", "value" => "solved" }]
-  }
+  it_should_be_readable :user_views
   it_should_be_deletable
 end
