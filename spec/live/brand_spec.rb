a00ae6a1238b26fb9adf89db6d421c498336e764
Steven Davidovitz <sdavidovitz@zendesk.com>
2014-08-13 18:35:13 -0700

    brand live spec fixes

--- spec/live/brand_spec.rb
+++ spec/live/brand_spec.rb
@@ -2,11 +2,13 @@ require 'core/spec_helper'
 
 describe ZendeskAPI::Brand, :delete_after do
   def valid_attributes
-    { :name => "Awesome-O" }
+    { :name => "Awesome-O", :subdomain => "zendeskapi-#{Time.now.to_i}" }
   end
 
   it_should_be_creatable
   it_should_be_updatable :name
-  it_should_be_deletable
-  it_should_be_readable :brands, :create => true
+  it_should_be_readable :brands
+
+  # TODO: needs to be set inactive first
+  # it_should_be_deletable
 end
