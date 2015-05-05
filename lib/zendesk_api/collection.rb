17d9ba081eb0a19951904cbd0e2040bbb4a2de7f
Steven Davidovitz <sdavidovitz@zendesk.com>
2015-05-05 10:47:24 -0700

    fix for incremental infinite looping

--- lib/zendesk_api/collection.rb
+++ lib/zendesk_api/collection.rb
@@ -117,7 +117,7 @@ module ZendeskAPI
     end
 
     def last_page?
-      !@next_page
+      !@next_page || @next_page == @query
     end
 
     # Saves all newly created resources stored in this collection.
@@ -172,7 +172,6 @@ module ZendeskAPI
       @response = get_response(@query || self.path)
       handle_response(@response.body)
 
-      @query = nil
       @resources
     end
 
