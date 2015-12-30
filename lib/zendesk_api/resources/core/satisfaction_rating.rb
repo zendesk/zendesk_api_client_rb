3da10ed77ebb25a48c828fd07bf3b49b6db1a57b
Steven Davidovitz <sdavidovitz@zendesk.com>
2015-12-30 13:49:21 -0800

    satisfaction ratings

--- lib/zendesk_api/resources/core/satisfaction_rating.rb
+++ lib/zendesk_api/resources/core/satisfaction_rating.rb
@@ -1,5 +1,15 @@
 module ZendeskAPI
   class SatisfactionRating < ReadResource
+    self.resource_name = 'satisfaction_ratings'
+    self.singular_resource_name = 'satisfaction_rating'
+
+    self.collection_paths = [
+      'satisfaction_ratings',
+      'satisfaction_ratings/received'
+    ]
+
+    self.resource_paths = ['satisfaction_ratings/%{id}']
+
     has :assignee, class: 'User'
     has :requester, class: 'User'
     has :ticket, class: 'Ticket'
