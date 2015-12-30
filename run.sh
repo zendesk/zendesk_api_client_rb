df33a19f52b81649259c6a7fcc0ec453a19e78e1
Steven Davidovitz <sdavidovitz@zendesk.com>
2015-12-30 13:00:53 -0800

    .json as a suffix came from ... somewhere

--- run.sh
+++ run.sh
@@ -5,6 +5,11 @@ set -e
 
 #git clean -xdf -- spec/fixtures/cassettes/*
 #(git ls-files spec/fixtures/cassettes/ | xargs sed -i'' -e "s/http:\/\/agent%40zendesk\.com:Testing123%21@support\.localhost\([^\"]*\)/https:\/\/sdavidovitz%2Bqa%40zendesk.com:123456@zendeskapi\.zd-staging\.com\1/g")
+# https://sdavidovitz%2Bqa%40zendesk.com:123456@zendeskapi.zd-staging.com/api/v2/automations/26633472.json
+# git ls-files spec/fixtures/cassettes/ | xargs sed -i '.bak' -e "s/\(https:\/\/sdavidovitz%2Bqa%40zendesk\.com:123456@[^\.]*\)\.json/\1/g"
+#(git ls-files spec/fixtures/cassettes/ | xargs sed -i '.bak' -e "s/\(https:\/\/sdavidovitz[^\"]*\)\.json/\1/g" spec/fixtures/cassettes/ZendeskAPI_Automation/deletion/should_be_destroyable.json) && \
+#  git clean -xfd -- spec/fixtures/cassettes/
+
 
 #echo 'username: "sdavidovitz+qa@zendesk.com"' > spec/fixtures/credentials.yml
 #echo "token: \"$OAUTH_TOKEN\"" >> spec/fixtures/credentials.yml
