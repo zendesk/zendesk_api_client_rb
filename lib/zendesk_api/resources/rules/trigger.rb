5e73c8a25cc1fd36903cb88201d3abb3cb3eea21
Steven Davidovitz <sdavidovitz@zendesk.com>
2015-12-30 14:19:01 -0800

    triggers

--- lib/zendesk_api/resources/rules/trigger.rb
+++ lib/zendesk_api/resources/rules/trigger.rb
@@ -7,6 +7,16 @@ module ZendeskAPI
     include Conditions
     include Actions
 
+    self.resource_name = 'triggers'
+    self.singular_resource_name = 'trigger'
+
+    self.collection_paths = [
+      'triggers',
+      'triggers/active'
+    ]
+
+    self.resource_paths = ['triggers/%{id}']
+
     has :execution, class: 'RuleExecution'
   end
 end
