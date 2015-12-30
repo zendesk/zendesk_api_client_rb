845068ad4f3251a56bec3b4a207bb803118b1bcb
Steven Davidovitz <sdavidovitz@zendesk.com>
2015-12-30 13:33:52 -0800

    macros

--- lib/zendesk_api/resources/rules/macro.rb
+++ lib/zendesk_api/resources/rules/macro.rb
@@ -8,16 +8,26 @@ module ZendeskAPI
     self.resource_name = 'macros'
     self.singular_resource_name = 'macro'
 
+    self.collection_paths = [
+      'macros',
+      'macros/active'
+    ]
+
+    self.resource_paths = [
+      'macros/%{id}'
+    ]
+
     has :execution, class: 'RuleExecution'
 
     # Returns the update to a ticket that happens when a macro will be applied.
     # @param [Ticket] ticket Optional {Ticket} to apply this macro to.
     # @raise [Faraday::Error::ClientError] Raised for any non-200 response.
     def apply!(ticket = nil)
-      path = "#{self.path}/apply"
+      path = "#{self.path.format(attributes)}/apply"
 
       if ticket
-        path = "#{ticket.path}/#{path}"
+        # TODO expose this nicely somehow
+        path = "#{ticket.path.format(ticket.attributes)}/#{path}"
       end
 
       response = @client.connection.get(path)
