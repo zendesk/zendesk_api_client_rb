d1b78655d578e13b07e04862a8918ca7bb7923f1
Steven Davidovitz <sdavidovitz@zendesk.com>
2015-12-30 14:00:45 -0800

    targets

--- lib/zendesk_api/resources/core/target.rb
+++ lib/zendesk_api/resources/core/target.rb
@@ -1,4 +1,9 @@
 module ZendeskAPI
   class Target < Resource
+    self.resource_name = 'targets'
+    self.singular_resource_name = 'target'
+
+    self.collection_paths = ['targets']
+    self.resource_paths = ['targets/%{id}']
   end
 end
