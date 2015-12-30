a8c43209f63aa5bf368652f23b358ccec2ecdc1d
Steven Davidovitz <sdavidovitz@zendesk.com>
2015-12-30 14:36:48 -0800

    user views

--- lib/zendesk_api/resources/rules/user_view.rb
+++ lib/zendesk_api/resources/rules/user_view.rb
@@ -1,5 +1,12 @@
 module ZendeskAPI
   class UserView < Rule
+    self.resource_name = 'user_views'
+    self.singular_resource_name = 'user_view'
+
+    self.collection_paths = ['user_views']
+    self.resource_paths = ['user_views/%{id}']
+
+    # TODO
     def self.preview(client, options = {})
       Collection.new(client, UserViewRow, options.merge!(:path => "user_views/preview", :verb => :post))
     end
