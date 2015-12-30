e14e73654c760ea08e015a7dadac6ec0235a66b0
Steven Davidovitz <sdavidovitz@zendesk.com>
2015-12-30 13:05:49 -0800

    fix bookmarks, try and fix brands (? bad spec)

--- lib/zendesk_api/resources/core/bookmark.rb
+++ lib/zendesk_api/resources/core/bookmark.rb
@@ -1,4 +1,8 @@
 module ZendeskAPI
   class Bookmark < Resource
+    self.resource_name = 'bookmarks'
+    self.singular_resource_name = 'bookmark'
+    self.collection_paths = ['bookmarks']
+    self.resource_paths = ['bookmarks/%{id}']
   end
 end
--- lib/zendesk_api/resources/core/brand.rb
+++ lib/zendesk_api/resources/core/brand.rb
@@ -1,5 +1,10 @@
 module ZendeskAPI
   class Brand < Resource
+    self.resource_name = 'brands'
+    self.singular_resource_name = 'brand'
+    self.collection_paths = ['brands']
+    self.resource_paths = ['brands/%{id}']
+
     def destroy!
       self.active = false
       save!
