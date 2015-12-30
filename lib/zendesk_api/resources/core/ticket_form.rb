a0dfff637ef0cc43e9e28a1af20e437f6a1fa186
Steven Davidovitz <sdavidovitz@zendesk.com>
2015-12-30 14:02:17 -0800

    ticket forms

--- lib/zendesk_api/resources/core/ticket_form.rb
+++ lib/zendesk_api/resources/core/ticket_form.rb
@@ -1,5 +1,10 @@
 module ZendeskAPI
   class TicketForm < Resource
+    self.resource_name = 'ticket_forms'
+    self.singular_resource_name = 'ticket_form'
+    self.collection_paths = ['ticket_forms']
+    self.resource_paths = ['ticket_forms/%{id}']
+
     # TODO
     # post :clone
   end
