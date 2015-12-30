4ebf5badc75aa924365b776f2a3e256503834c34
Steven Davidovitz <sdavidovitz@zendesk.com>
2015-12-30 13:29:42 -0800

    locales

--- lib/zendesk_api/resources/core/locale.rb
+++ lib/zendesk_api/resources/core/locale.rb
@@ -1,4 +1,14 @@
 module ZendeskAPI
   class Locale < ReadResource
+    self.resource_name = 'locales'
+    self.singular_resource_name = 'locale'
+
+    self.collection_paths = [
+      'locales',
+      'locales/public',
+      'locales/agent'
+    ]
+
+    self.resource_paths = ['locales/%{id}'] # TODO support detect_best_locale, current_locale
   end
 end
