17e8faff639e3eca5472fd173f4ce5f30d388c8a
Steven Davidovitz <sdavidovitz@zendesk.com>
2015-12-30 14:38:10 -0800

    phone numbers

--- lib/zendesk_api/resources/voice/phone_number.rb
+++ lib/zendesk_api/resources/voice/phone_number.rb
@@ -4,6 +4,9 @@ module ZendeskAPI
       self.resource_name = 'phone_numbers'
       self.singular_resource_name = 'phone_number'
 
+      self.collection_paths = ['channels/voice/phone_numbers']
+      self.resource_paths = ['channels/voice/phone_numbers/%{id}']
+
       namespace "channels/voice"
     end
   end
