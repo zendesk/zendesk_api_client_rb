2201b849101624edf23747b8c4989da533a96202
Steven Davidovitz <sdavidovitz@zendesk.com>
2015-12-30 11:56:56 -0800

    don't sed out random stuff anymore

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -e
 # bundle
 
 #git clean -xdf -- spec/fixtures/cassettes/*
-(git ls-files spec/fixtures/cassettes/ | xargs sed -i'' -e "s/http:\/\/agent%40zendesk\.com:Testing123%21@support\.localhost\([^\"]*\)/https:\/\/sdavidovitz%2Bqa%40zendesk.com:123456@zendeskapi\.zd-staging\.com\1/g")
+#(git ls-files spec/fixtures/cassettes/ | xargs sed -i'' -e "s/http:\/\/agent%40zendesk\.com:Testing123%21@support\.localhost\([^\"]*\)/https:\/\/sdavidovitz%2Bqa%40zendesk.com:123456@zendeskapi\.zd-staging\.com\1/g")
 
 #echo 'username: "sdavidovitz+qa@zendesk.com"' > spec/fixtures/credentials.yml
 #echo "token: \"$OAUTH_TOKEN\"" >> spec/fixtures/credentials.yml
