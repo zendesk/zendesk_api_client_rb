5e2541713f88ba31b33ab173e1a5f9343b8d4842
Steven Davidovitz <sdavidovitz@zendesk.com>
2015-12-30 13:25:18 -0800

    group_membership

--- lib/zendesk_api/resources/core/group.rb
+++ lib/zendesk_api/resources/core/group.rb
@@ -1,10 +1,21 @@
 module ZendeskAPI
   class Group < Resource
+    self.resource_name = 'groups'
+    self.singular_resource_name = 'group'
+
+    self.collection_paths = ['groups']
+    self.resource_paths = ['groups/%{id}']
   end
 
   class GroupMembership < Resource
     extend CreateMany
     extend DestroyMany
+
+    self.resource_name = 'group_memberships'
+    self.singular_resource_name = 'group_membership'
+
+    self.collection_paths = ['group_memberships']
+    self.resource_paths = ['group_memberships/%{id}']
 
     has :user, class: 'User'
     has :group, class: 'Group'
