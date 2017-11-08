f1ad8ba0f9c63e073722cdc05f43c38dc092413f
otoyo <hiroki.t.1988@gmail.com>
2017-11-08 16:36:27 +0900

    Use Range#cover? instead of Range#include?

--- lib/zendesk_api/middleware/response/logger.rb
+++ lib/zendesk_api/middleware/response/logger.rb
@@ -19,7 +19,7 @@ module ZendeskAPI
 
           @app.call(env).on_complete do |env|
             info = "Status #{env[:status].to_s}"
-            info.concat(" #{env[:body].inspect}") if (400..499).include?(env[:status].to_i)
+            info.concat(" #{env[:body].inspect}") if (400..499).cover?(env[:status].to_i)
 
             @logger.info info
             @logger.debug dump_debug(env, :response_headers)
