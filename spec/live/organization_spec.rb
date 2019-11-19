405f1a9f2f7052e046de2d2c8d7301a874c65b99
David Tapiador <dtapiador@zendesk.com>
2019-11-19 12:10:24 +1100

    add integration tests for bulk organization actions

--- spec/live/organization_spec.rb
+++ spec/live/organization_spec.rb
@@ -9,4 +9,66 @@ describe ZendeskAPI::Organization, :delete_after do
   it_should_be_updatable :name
   it_should_be_deletable
   it_should_be_readable :organizations, :create => true
+
+  describe "create_or_update" do
+    after do
+      VCR.use_cassette("create_or_update_destroy_organization") do
+        organization.destroy
+      end
+    end
+
+    context "when the organization already exist" do
+      let!(:organization) do
+        VCR.use_cassette("create_or_update_create_organization") do
+          client.organizations.create(name: "Existing", external_id: "100")
+        end
+      end
+
+      before do
+        VCR.use_cassette("create_or_update_existing_organization") do
+          ZendeskAPI::Organization.create_or_update!(client, name: "Updated!", external_id: "100")
+        end
+      end
+
+      it "updates the existing user" do
+        VCR.use_cassette("create_or_update_find_existing_organization") do
+          expect(client.organizations.find(id: organization.id).name).to eql "Updated!"
+        end
+      end
+    end
+  end
+
+  describe "create_many and destroy_many" do
+    let(:create_many_job) do
+      VCR.use_cassette("create_many_organizations_job") do
+        ZendeskAPI::Organization.create_many!(
+          client,
+          [
+            { name: "one", external_id: "101" },
+            { name: "two", external_id: "102" }
+          ]
+        ).tap do |job|
+          job.reload! while job.status != "completed"
+        end
+      end
+    end
+
+    let(:destroy_many_job) do
+      VCR.use_cassette("destroy_many_organizations_job") do
+        ZendeskAPI::Organization.destroy_many!(
+          client,
+          create_many_job.results.map { |result| result["id"] }
+        ).tap do |job|
+          job.reload! while job.status != "completed"
+        end
+      end
+    end
+
+    it "creates many and then it can destroy many" do
+      created_orgs = create_many_job.results.filter { |x| x["status"] == "Created" }
+
+      expect(created_orgs.count).to be 2
+      expect(destroy_many_job["total"]).to be 2
+    end
+  end
 end
