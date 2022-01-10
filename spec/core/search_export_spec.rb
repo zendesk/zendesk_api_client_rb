659975c5ab7a352217631d0ac8e88914ea3f74fc
Erik Trapin (ecoologic) <erik.trapin@zendesk.com>
2022-01-10 10:52:32 +1000

    search_export_spec refactor for readability

--- spec/core/search_export_spec.rb
+++ spec/core/search_export_spec.rb
@@ -1,21 +1,21 @@
 require 'core/spec_helper'
 
 describe ZendeskAPI::SearchExport do
-  context ".new" do
+  describe ".new" do
     context "when given an existing class" do
-      it "should return the correct class" do
+      it "returns an instance of the specific class" do
         expect(ZendeskAPI::SearchExport.new(nil, "result_type" => "user")).to be_instance_of(ZendeskAPI::User)
       end
     end
 
     context "when given a nonexistent class" do
-      it "should return an object of the type Search::Result" do
+      it "returns an instance of the generic Search::Result" do
         expect(ZendeskAPI::SearchExport.new(nil, "result_type" => "blah")).to be_instance_of(ZendeskAPI::SearchExport::Result)
       end
     end
 
     context "when not given anything" do
-      it "should return an object of the type Search::Result" do
+      it "returns an instance of Search::Result by default" do
         expect(ZendeskAPI::SearchExport.new(nil, {})).to be_instance_of(ZendeskAPI::SearchExport::Result)
       end
     end
