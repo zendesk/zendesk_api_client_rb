0fddaa8e57744ad1f5aa40e826ac898dc64c3834
Erik Trapin (ecoologic) <erik.trapin@zendesk.com>
2022-01-10 10:21:43 +1000

    spec: linting

--- spec/core/search_export_spec.rb
+++ spec/core/search_export_spec.rb
@@ -4,13 +4,13 @@ describe ZendeskAPI::SearchExport do
   context ".new" do
     context "when given an existing class" do
       it "should return the correct class" do
-        expect(ZendeskAPI::SearchExport.new(nil, { "result_type" => "user" })).to be_instance_of(ZendeskAPI::User)
+        expect(ZendeskAPI::SearchExport.new(nil, "result_type" => "user")).to be_instance_of(ZendeskAPI::User)
       end
     end
 
     context "when given a nonexistent class" do
       it "should return an object of the type Search::Result" do
-        expect(ZendeskAPI::SearchExport.new(nil, { "result_type" => "blah" })).to be_instance_of(ZendeskAPI::SearchExport::Result)
+        expect(ZendeskAPI::SearchExport.new(nil, "result_type" => "blah")).to be_instance_of(ZendeskAPI::SearchExport::Result)
       end
     end
 
