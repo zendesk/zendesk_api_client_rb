ec6021740060e08124a30059e697d313477dd433
Jin Huang <jin.huang@zendesk.com>
2023-01-23 16:03:52 +1100

    update tag_spec to reflect tag changes are now using "PUT"

--- spec/live/tag_spec.rb
+++ spec/live/tag_spec.rb
@@ -7,44 +7,45 @@ RSpec.describe ZendeskAPI::Tag, :vcr, :not_findable do
     under object do
       before do
         parent.tags = %w{tag2 tag3}
+        let!(:existing_tags) {tags | parent.tags}
         parent.tags.save!
       end
 
       it "can be set" do
-        expect(tags).to eq(%w{tag2 tag3})
+        expect(tags).to eq(:existing_tags)
       end
 
       it "should be removable" do
         parent.tags.destroy!(:id => "tag2")
 
-        expect(tags).to eq(%w{tag3})
+        expect(tags).to eq(:existing_tags.delete("tag2"))
       end
 
       it "shouldn't re-save destroyed tags" do
         parent.tags.first.destroy!
         parent.tags.save!
 
-        expect(tags).to eq(%w{tag3})
+        expect(tags).to eq(:existing_tags.delete_at(0))
       end
 
       it "should be updatable" do
         parent.tags.update!(:id => "tag4")
 
-        expect(tags).to eq(%w{tag2 tag3 tag4})
+        expect(tags).to eq(:existing_tags << "tag4")
       end
 
       it "should be savable" do
         parent.tags << "tag4"
         parent.tags.save!
 
-        expect(tags).to eq(%w{tag2 tag3 tag4})
+        expect(tags).to eq(:existing_tags << "tag4")
       end
 
       it "should be modifiable" do
         parent.tags.delete(ZendeskAPI::Tag.new(nil, :id => "tag2"))
         parent.tags.save!
 
-        expect(tags).to eq(%w{tag3})
+        expect(tags).to eq(:existing_tags.delete("tag2"))
 
         parent.tags.delete_if { |tag| tag.id == "tag3" }
         parent.tags.save!
